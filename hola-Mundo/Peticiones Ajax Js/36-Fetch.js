502b929a625698dd71b7824c08fcd0df2e9bc0ba
Cruz Ibarra <jackzelda_vicio@hotmail.com>
2020-09-29 15:18:43 -0700

    Added New Examples

--- hola-Mundo/Peticiones Ajax Js/36-Fetch.js
+++ hola-Mundo/Peticiones Ajax Js/36-Fetch.js
@@ -1,21 +1,81 @@
 'use strict'
 
-// Fetch (Ajax) y peticiones a servicios / Apis Rest
 var divUsuarios = document.querySelector('#usuarios');
-var usuarios = [];
+var divSingleUser = document.querySelector('#singleUser');
 
-fetch('https://reqres.in/api/users?page=1')
+getFetch().then(data => data.json())
+    .then(user => {
+        getUsers(user.data);
+
+        return getOneUserFetch();
+
+    })
     .then(data => data.json())
-    .then(users => {
-        usuarios = users.data;
-        console.log(usuarios);
+    .then(singleUser => {
+        getSingleUser(singleUser.data);
 
-        usuarios.map((user, i) => {
-            var li = document.createElement('li');
-
-            li.innerHTML = user.email;
-
-            divUsuarios.appendChild(li);
-        });
     })
 
+
+
+
+function getFetch() {
+    return fetch('https://reqres.in/api/users?page=2')
+}
+
+function getOneUserFetch() {
+    return fetch('https://reqres.in/api/users/2');
+}
+
+function getInfo() {
+
+    var profesor = {
+        nombre: 'Victor',
+        apellidos: 'Robles',
+        url: 'http://www.victorroblesweb.es'
+    };
+
+    return new Promise((resolve, reject) => {
+        var profesor_string = JSON.stringify(profesor);
+
+        if(typeof profesor_string != 'string') return reject('error');
+
+        return resolve(profesor_string);
+    })
+
+
+
+
+}
+
+
+// WE GET ALL USER
+function getUsers(user) {
+
+    user.map((index, i) => {
+
+        var li = document.createElement('li');
+
+        li.innerHTML = (i + 1) + " " + index.email;
+
+        divUsuarios.append(li);
+
+        document.querySelector('#loading').style.display = 'none'
+    })
+
+}
+
+// WE GET JUST ONE USER
+function getSingleUser(user) {
+
+
+    var li = document.createElement('li');
+
+    li.innerHTML = user.email;
+    li.style.color = "red";
+
+    divSingleUser.append(li);
+
+
+
+}
