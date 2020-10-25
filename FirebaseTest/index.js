7687af2a1d27f86f8754efc90c7338ecb072db16
Cruz Ibarra <jackzelda_vicio@hotmail.com>
2020-10-24 17:46:07 -0700

    Added Firebase Test

--- FirebaseTest/index.js
+++ FirebaseTest/index.js
@@ -1,1 +1,16 @@
 'use strict'
+
+const db  = firebase.firestore();
+
+var form = document.querySelector("#form")
+
+form.addEventListener('submit', e =>{
+    var inputCollection = document.querySelector("#inputCollection").value;
+    var inputTitle = document.querySelector("#inputTitle").value;
+    var inputText = document.querySelector("#inputText").value;
+
+    db.collection(`${inputCollection}`).doc("uno").set({
+        inputTitle,
+        inputText
+    })
+})
