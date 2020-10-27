0094b95bf9be15abd7005c5138e7b76cede71b89
Cruz Ibarra <jackzelda_vicio@hotmail.com>
2020-10-27 10:16:00 -0700

    Added Get Data from  firebase

--- FirebaseTest/index.js
+++ FirebaseTest/index.js
@@ -1,18 +1,39 @@
-'use strict'
+"use strict";
 
-const db =  firebase.firestore();
+const db = firebase.firestore();
 
 const form = document.querySelector('#form');
+const formTareas = document.querySelector('#tareas');
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
+docTitle.focus();
 
-    let docName = document.querySelector('#docName').value;
-    let docTitle = document.querySelector('#docTitle').value;
-    let docText = document.querySelector('#docText').value;
-    
-    var datos = [docName,docTitle,docText]
-    db.collection(`${docTitle}`).doc(`${docName}`).set({
-         datos
+const saveData = (titulo, texto, baseDatos) => {
+  db.collection("tareas").doc(`${titulo}`).set({
+    texto,
+  });
+};
+
+const getData = () => db.collection("tareas").get();
+
+window.addEventListener("DOMContentLoaded", async (e) => {
+    const querySnapshot = await getData();
+    querySnapshot.forEach(doc => {
+        console.log(doc.data())
+        formTareas.innerHTML += `<h4>${doc.data().texto}</h4>`
     })
-})
+});
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  let baseDatos = document.querySelector("#docName");
+  let titulo = document.querySelector("#docTitle");
+  let texto = document.querySelector("#docText");
+
+  await saveData(titulo.value, texto.value, baseDatos.value);
+
+  form.reset();
+  docTitle.focus();
+
+  console.log(e);
+});
