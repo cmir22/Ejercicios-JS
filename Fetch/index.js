0416a71413f746e767dce5124f9bc369e43cdbe7
Cruz Ibarra <jackzelda_vicio@hotmail.com>
2020-11-16 20:19:10 -0800

    Updated Files

--- Fetch/index.js
+++ Fetch/index.js
@@ -1,23 +1,24 @@
-'use strict'
+"use strict";
 
-const divUsuarios = document.querySelector('#usuarios');
-var usuarios = [];
+const divUsuarios = document.querySelector("#usuarios");
+var form = document.querySelector("#form");
 
-getFetch().then(data => data.json()).then(function (users) {
-  getUsers(users)
-  
-})
+form.addEventListener("submit", () => {
+  var inputText = document.querySelector("#inputText").value;
+  getFetch()
+    .then((data) => data.json())
+    .then((user) => {
+      var usuarios = user.data;
+      usuarios.map((index) => {
+        if (inputText === index.email) {
+          let li = document.createElement("li");
+          li.innerHTML = index.email;
+          divUsuarios.append(li);
+        }
+      });
+    });
+});
 
-function getFetch(){
-  return fetch('https://jsonplaceholder.typicode.com/users')
+function getFetch() {
+  return fetch("https://reqres.in/api/users?page=2");
 }
-
-function getUsers(users){
-users.map(index =>{
-  let li = document.createElement('p');
-  li.innerHTML = index.name;
-  divUsuarios.append(li)
-})
-}
-
-
