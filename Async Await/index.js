1595921a092732b0b951c8b11726ef05dc0f1bee
cmir22 <jackzelda_vicio@hotmail.com>
2020-12-21 14:51:28 -0800

    Added async await exercise

--- Async Await/index.js
+++ Async Await/index.js
@@ -1,17 +1,34 @@
-'use strict'
+"use strict";
 
 const getData = async (idPost) => {
-    const datos = await fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}`)
+  const datos = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${idPost}`
+  );
 
-    const datosJson = await datos.json();
-    
-    console.log(datosJson.id)
+  const datosJson = await datos.json();
 
-    const usuario = await fetch(`https://jsonplaceholder.typicode.com/users/${datosJson.id}`)
+  console.log(datosJson.id);
 
-    const usuarioJson = await usuario.json();
+  const usuario = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${datosJson.id}`
+  );
 
-    console.log(usuarioJson.name)
+  const usuarioJson = await usuario.json();
+
+  const divUsuario = document.querySelector("#usuario");
+  
+  var h3 = document.createElement("h3");
+  h3.innerHTML = usuarioJson.name;
+  divUsuario.append(usuarioJson.name);
+};
+
+function searchUser() {
+  const form = document.querySelector("#form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let inputValue = document.querySelector("#inputValue");
+    getData(inputValue.value);
+  });
 }
 
-getData(4)
+searchUser();
