2247ff4a9f650350d4f5cfdede906ee66b58a303
cmir22 <jackzelda_vicio@hotmail.com>
2020-12-23 14:11:40 -0800

    Added Readme info and extended PokeAPI

--- Api Pokemon/index.js
+++ Api Pokemon/index.js
@@ -7,6 +7,15 @@ const apiPokemon = async (pokemon) => {
     const getPokemon = await setPokemon.json();
 
     var habilidades = getPokemon.abilities
+
+    var pokemonId = getPokemon.id
+
+    const setPokemonForm = await fetch(`https://pokeapi.co/api/v2/pokemon-form/${pokemonId}`)
+
+    const getPokemonForm = await setPokemonForm.json();
+
+    var getPokemonImg = getPokemonForm.sprites.front_default
+
     
     habilidades.map(element => {
         const divCards = document.querySelector('#divCards');
@@ -16,11 +25,13 @@ const apiPokemon = async (pokemon) => {
         var ability = element.ability.name
         div.innerHTML = `
         <h5>${ability}</h5>
+        <img src="${getPokemonImg}">
         `  
         divCards.append(div)
+        console.log(ability)
          
     });
 
 }
 
-apiPokemon('charizard')
+apiPokemon('ivysaur')
