cee060bae9bba344f182760e50c109b285c8be95
cmir22 <jackzelda_vicio@hotmail.com>
2020-12-28 11:15:52 -0800

    PikeAPI Searcher done with ussues fixed

--- Api Pokemon/index.js
+++ Api Pokemon/index.js
@@ -1,37 +1,36 @@
-'use strict'
+"use strict";
 
-const apiPokemon = async (pokemon) => {
-
-    const setPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-
-    const getPokemon = await setPokemon.json();
-
-    var habilidades = getPokemon.abilities
-
-    var pokemonId = getPokemon.id
-
-    const setPokemonForm = await fetch(`https://pokeapi.co/api/v2/pokemon-form/${pokemonId}`)
-
-    const getPokemonForm = await setPokemonForm.json();
-
-    var getPokemonImg = getPokemonForm.sprites.front_default
-
-    
-    habilidades.map(element => {
-        const divCards = document.querySelector('#divCards');
-        var div = document.createElement('div')
-        div.className = 'card'
-        div.id = 'carta'
-        var ability = element.ability.name
-        div.innerHTML = `
-        <h5>${ability}</h5>
-        <img src="${getPokemonImg}">
-        `  
-        divCards.append(div)
-        console.log(ability)
-         
-    });
-
+async function getPokemon(id) {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = await response.json();
+    return await data;
+  } catch (err) {
+    console.log("No se encontro el Pokemon");
+  }
 }
 
-apiPokemon('ivysaur')
+async function init() {
+  const form = document.getElementById("form");
+
+  form.addEventListener("keyup", async (event) => {
+    event.preventDefault();
+    var inputSearch = document.querySelector("#inputSearch").value;
+    const pokemon = await getPokemon(inputSearch);
+    console.log(window.pokemonDisplay.pokemonName);
+
+    try {
+      window.pokemonName.textContent = pokemon.name;
+      window.pokemonImg.setAttribute("src", pokemon.sprites.front_default);
+    } catch (err) {
+      window.pokemonImg.setAttribute(
+        "src",
+        "https://cdn.iconscout.com/icon/free/png-256/page-not-found-5-530376.png"
+      );
+      window.pokemonName.textContent = "Pokemon";
+      //console.log("Pokemon no Encontrado");
+    }
+  });
+}
+
+init();
