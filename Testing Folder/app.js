f4d78e122c3a7d914d0e5105b4a1a29a4e2e270c
cmir22 <jackzelda_vicio@hotmail.com>
2021-01-15 12:59:23 -0800

    Reqres tested using async await

--- Testing Folder/app.js
+++ Testing Folder/app.js
@@ -3,24 +3,22 @@
 const getUsers = async (id) => {
   const response = await fetch(`https://reqres.in/api/users?page=2`);
   const responseJson = await response.json();
-  const users = responseJson.data;
-  displayUsers(users);
-};
+  const data = responseJson.data;
 
-function displayUsers(users) {
-  var usuarios = document.querySelector(".usuarios");
-  users.map((element) => {
-    var divUser = document.createElement("div");
-    var image = document.createElement("img");
-    divUser.classList = "divUser";
-    divUser.innerHTML = `
-      <h2>Number: ${element.id}</h2>
-      <img src="${element.avatar}" />
+  data.map((element) => {
+    const divUsers = document.getElementById("users");
+    let divContainer = document.createElement("div");
 
+    divContainer.innerHTML = `
+    <div id="divi">
+      <img src="${element.avatar}" id="image">
+      <h1> ${element.first_name}</h1>
+    </div>
       `;
-    usuarios.append(divUser);
+
+    divUsers.append(divContainer);
   });
-}
+};
 
 function init() {
   getUsers();
